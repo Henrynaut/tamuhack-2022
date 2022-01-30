4ded1464a63506ff600a57efbb85e2c304d3a6c5
Abhishek22002 <41200654+Abhishek22002@users.noreply.github.com>
2022-01-29 22:18:03 -0600

    Added a backend test env.

--- angular-integration-example/src/app/app-routing.module.ts
+++ angular-integration-example/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ChooseFlightComponent } from './chooseFlight/chooseFlight.component';
-
+import { TestViewComponent } from './backend/test-view/test-view.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'chooseFlight', component: ChooseFlightComponent }
+  { path: 'chooseFlight', component: ChooseFlightComponent },
+  { path: 'test-view', component: TestViewComponent }
 ]
 
 @NgModule({
