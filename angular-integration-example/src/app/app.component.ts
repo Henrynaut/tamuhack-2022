1b8e1c8bbb83fa7efa9f8f0e8452b89d179f7e48
Henrynaut <neilmchenry@gmail.com>
2022-01-29 23:12:27 -0600

    Update app.component.ts

--- angular-integration-example/src/app/app.component.ts
+++ angular-integration-example/src/app/app.component.ts
@@ -5,11 +5,17 @@ import{UsersService}from './users.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-integration';
   constructor(private user:UsersService){
-    this.user.getData().subscribe(data=>{
+    var obj = this.user.getData().subscribe(data=>{
       console.warn(data);
     })
+
+    // var str:string = JSON.stringify(obj);
+    // str = JSON.stringify(obj, null, 4); // (Optional) beautiful indented output.
+    // console.log(str); // Logs output to dev tools console.
+    // alert(str); // Displays output using window.alert()
   }
 }
